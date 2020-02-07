a716467358d9cdeb42bb1dff671353fd234f3c3e
Gustavo Lelis <galelis@gmail.com>
2020-02-07 16:09:46 -0300

    Creating w/l preset commands

--- platform/viewer/src/appExtensions/GenericViewerCommands/commandsModule.js
+++ platform/viewer/src/appExtensions/GenericViewerCommands/commandsModule.js
@@ -1,5 +1,5 @@
-import { redux } from "@ohif/core";
-import store from "./../../store";
+import { redux } from '@ohif/core';
+import store from './../../store';
 const { setViewportActive } = redux.actions;
 
 const actions = {
@@ -16,24 +16,74 @@ const actions = {
     newIndex = newIndex < 0 ? maxIndex : newIndex;
 
     store.dispatch(setViewportActive(newIndex));
-  }
+  },
+  setWindowLevelPreset: ({ preset }) => {
+    const wlPresetData = store.getState().preferences.windowLevelData[preset];
+    // TODO: Change window level of viewers
+  },
 };
 
 const definitions = {
   // Next/Previous active viewport
   incrementActiveViewport: {
     commandFn: actions.updateActiveViewport,
-    storeContexts: ["viewports"],
-    options: { direction: 1 }
+    storeContexts: ['viewports'],
+    options: { direction: 1 },
   },
   decrementActiveViewport: {
     commandFn: actions.updateActiveViewport,
-    storeContexts: ["viewports"],
-    options: { direction: -1 }
-  }
+    storeContexts: ['viewports'],
+    options: { direction: -1 },
+  },
+  // Window level Presets
+  windowLevelPreset1: {
+    commandFn: actions.setWindowLevelPreset,
+    storeContexts: [],
+    options: { preset: 1 },
+  },
+  windowLevelPreset2: {
+    commandFn: actions.setWindowLevelPreset,
+    storeContexts: [],
+    options: { preset: 2 },
+  },
+  windowLevelPreset3: {
+    commandFn: actions.setWindowLevelPreset,
+    storeContexts: [],
+    options: { preset: 3 },
+  },
+  windowLevelPreset4: {
+    commandFn: actions.setWindowLevelPreset,
+    storeContexts: [],
+    options: { preset: 4 },
+  },
+  windowLevelPreset5: {
+    commandFn: actions.setWindowLevelPreset,
+    storeContexts: [],
+    options: { preset: 5 },
+  },
+  windowLevelPreset6: {
+    commandFn: actions.setWindowLevelPreset,
+    storeContexts: [],
+    options: { preset: 6 },
+  },
+  windowLevelPreset7: {
+    commandFn: actions.setWindowLevelPreset,
+    storeContexts: [],
+    options: { preset: 7 },
+  },
+  windowLevelPreset8: {
+    commandFn: actions.setWindowLevelPreset,
+    storeContexts: [],
+    options: { preset: 8 },
+  },
+  windowLevelPreset9: {
+    commandFn: actions.setWindowLevelPreset,
+    storeContexts: [],
+    options: { preset: 9 },
+  },
 };
 
 export default {
   definitions,
-  defaultContext: "VIEWER"
+  defaultContext: 'VIEWER',
 };
